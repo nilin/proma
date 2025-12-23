38f990d4bd6962e703c3b449dd6c81d8c876aa60
Nilin Abrahamsen <nilinabra@gmail.com>
2025-12-22 21:22:03 -0800

    add another dim to test of unflatten

--- dump/test.py
+++ dump/test.py
@@ -1,13 +1,19 @@
 import pandas as pd
 
+a = pd.read_parquet('attention_mask_985772.parquet').to_numpy()
 x = pd.read_parquet('x_985772.parquet').to_numpy()
 flat_x = pd.read_parquet('flat_x_985772.parquet').to_numpy()
 unflat_x = pd.read_parquet('unflat_x_985772.parquet').to_numpy()
 
-print(x[0,x[0]!=0][:10])
+print(x.shape)
+print(flat_x.shape)
+print(unflat_x.shape)
+print(a.shape)
 
-print(flat_x[:10])
+print(x[0,x[0]!=0][:10,4])
 
-print(unflat_x[0,x[0]!=0][:10])
-print(unflat_x[0,unflat_x[0]!=0][:10])
+print(flat_x[:10,4])
+
+print(unflat_x[0,x[0]!=0][:10,4])
+print(unflat_x[0,unflat_x[0]!=0][:10,4])
 
