dcc8f96a867332161628438a98cb45447b336b90
Nilin Abrahamsen <nilinabra@gmail.com>
2026-02-06 06:43:48 +0000

    .

--- next.sh
+++ next.sh
@@ -1,14 +1,1 @@
 
-
-sudo docker run --rm --gpus all --shm-size=16g -e WANDB_API_KEY=$WANDB_API_KEY proma2 proma_intra_eig +actor_rollout_ref.actor.proma_shrinkage=1.0 ++trainer.experiment_name=proma_intra_eig_both-lr7 ++actor_rollout_ref.actor.optim.lr=7e-6 ++trainer.total_training_steps=102 ++actor_rollout_ref.actor.proma_intra_dim=100
-sudo fuser -k /dev/nvidia* 2>/dev/null || true
-
-sudo docker run --rm --gpus all --shm-size=16g -e WANDB_API_KEY=$WANDB_API_KEY proma2 grpo ++trainer.experiment_name=grpo-lr7 ++actor_rollout_ref.actor.optim.lr=7e-6 ++trainer.total_training_steps=102 
-sudo fuser -k /dev/nvidia* 2>/dev/null || true
-
-sudo docker run --rm --gpus all --shm-size=16g -e WANDB_API_KEY=$WANDB_API_KEY proma2 grpo ++trainer.experiment_name=grpo-lr7 ++actor_rollout_ref.actor.optim.lr=7e-6 ++trainer.total_training_steps=102 
-sudo fuser -k /dev/nvidia* 2>/dev/null || true
-
-sudo docker run --rm --gpus all --shm-size=16g -e WANDB_API_KEY=$WANDB_API_KEY proma2 grpo ++trainer.experiment_name=grpo-lr7 ++actor_rollout_ref.actor.optim.lr=7e-6 ++trainer.total_training_steps=102 
-sudo fuser -k /dev/nvidia* 2>/dev/null || true
-
